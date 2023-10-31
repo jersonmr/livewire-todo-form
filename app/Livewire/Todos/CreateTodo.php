e7d96d98c80c0a44404e92f9f8007c2718934a40
Jerson Moreno <jersonmr@gmail.com>
2023-10-31 09:42:21 -0400

    Add update and create todo validations

--- app/Livewire/Todos/CreateTodo.php
+++ app/Livewire/Todos/CreateTodo.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Todos;
 
 use App\Models\Todo;
 use Illuminate\Contracts\View\View;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class CreateTodo extends Component
@@ -14,8 +15,40 @@ class CreateTodo extends Component
 
     public bool $done = false;
 
+    public function rules(): array
+    {
+        return [
+            'title' => [
+                'required',
+                'min:3',
+                'max:10',
+                Rule::unique('todos', 'title')->where('user_id', auth()->id()),
+            ],
+            'description' => [
+                'required',
+                'min:3',
+                'max:100',
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'El título es requerido',
+            'title.min' => 'El título debe tener al menos :min caracteres',
+            'title.max' => 'El título debe tener como máximo :max caracteres',
+            'title.unique' => 'El título ya existe',
+            'description.required' => 'La descripción es requerida',
+            'description.min' => 'La descripción debe tener al menos :min caracteres',
+            'description.max' => 'La descripción debe tener como máximo :max caracteres',
+        ];
+    }
+
     public function save(): void
     {
+        $this->validate();
+
         Todo::create($this->only(['title', 'description', 'done']));
 
         session()->flash('status', 'La tarea se ha creado correctamente');
--- app/Livewire/Todos/UpdateTodo.php
+++ app/Livewire/Todos/UpdateTodo.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Todos;
 
 use App\Models\Todo;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class UpdateTodo extends Component
@@ -25,8 +26,42 @@ class UpdateTodo extends Component
         $this->done = $todo->done;
     }
 
+    public function rules(): array
+    {
+        return [
+            'title' => [
+                'required',
+                'min:3',
+                'max:10',
+                Rule::unique('todos', 'title')
+                    ->where('user_id', auth()->id())
+                    ->ignore($this->todo->id),
+            ],
+            'description' => [
+                'required',
+                'min:3',
+                'max:100',
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'El título es requerido',
+            'title.min' => 'El título debe tener al menos :min caracteres',
+            'title.max' => 'El título debe tener como máximo :max caracteres',
+            'title.unique' => 'El título ya existe',
+            'description.required' => 'La descripción es requerida',
+            'description.min' => 'La descripción debe tener al menos :min caracteres',
+            'description.max' => 'La descripción debe tener como máximo :max caracteres',
+        ];
+    }
+
     public function save(): void
     {
+        $this->validate();
+
         $this->todo->update($this->only(['title', 'description', 'done']));
 
         session()->flash('status', 'La tarea ha sido actualizada');
